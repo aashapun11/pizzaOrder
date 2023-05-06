c34cb2e8b2cd8276a715d2189045c4154203dc74
aashapun11 <106722144+aashapun11@users.noreply.github.com>
2023-05-06 17:09:31 +0530

    make home page

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ const path = require('path')
 const ejs = require("ejs");
 const expressLayout = require("express-ejs-layouts");
 
+// to use the static path
+const static_path = path.join(__dirname,"/public");
+app.use(express.static(static_path))
 
 // set Template engine
 const template_path = path.join(__dirname,"/resources/views")
 
-app.use(expressLayout)
+// app.use(expressLayout)
 app.set("view engine","ejs");
 app.set("views",template_path);
 
 const port = 3000 || process.env.PORT;
 app.get('/',(req,res)=>{
-    res.render("home")
+    res.render("home");
 })
 
 app.listen(port,()=>{
